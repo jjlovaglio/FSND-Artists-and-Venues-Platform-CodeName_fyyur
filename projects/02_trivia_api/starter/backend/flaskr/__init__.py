b162ff9a5676e12172a7fd1e6e231a82772e5b87
jjlovaglio <jjlovaglio@gmail.com>
2021-01-27 11:03:19 -0300

    /categories and /questions endpoints working.

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -14,19 +14,37 @@ def create_app(test_config=None):
   setup_db(app)
   
   '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs - done
   '''
+  CORS(app)
 
   '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
+  @TODO: Use the after_request decorator to set Access-Control-Allow - done
   '''
+
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
-  for all available categories.
+  for all available categories. - done
   '''
+  @app.route('/categories')
+  def get_categories():
+    categories = Category.query.order_by(Category.id).all()
+    formatted_categories = [category.format() for category in categories]
 
+    return jsonify({
+      'success': True,
+      'categories': formatted_categories,
+      'total_categories': len(categories)
+
+    })
 
   '''
   @TODO: 
@@ -40,6 +58,35 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  QUESTIONS_PER_PAGE = 10
+  def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+    return current_questions
+
+  @app.route('/questions', methods=['GET'])
+  def get_questions():
+    selection = Question.query.order_by(Question.id).all()
+    current_questions = paginate_questions(request, selection)
+
+    if len(current_questions) == 0:
+      abort(404)
+
+    categories = Category.query.order_by(Category.id).all()
+    formatted_categories = {category.id:category.type for category in categories}
+
+
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'categories': formatted_categories,
+      'current_category': None,
+      'total_questions': len(Question.query.all()),
+
+    })
 
   '''
   @TODO: 
